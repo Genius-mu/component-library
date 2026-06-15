03fb2d12bedc0a71ef3fc726abc92d8520c9a91f
Mustapha-Adegbite <adegbitemustapha247@gmail.com>
2026-06-15 09:54:15 +0100

    Rename to morgu, fix build/CSS/types/SSR, add 8 components + toast provider

---
+++ src/utils/cn.js
@@ -0,0 +1,6 @@
+﻿// utils/cn.js
+// Tiny, dependency-free className combiner. Filters out falsy values so you can
+// write cn("base", condition && "active", className) without leaking "false".
+export const cn = (...classes) => classes.filter(Boolean).join(" ");
+
+export default cn;
